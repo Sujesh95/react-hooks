11579aa952584462a07e3cf3576d802fc5f971e8
Sujesh95 <sujesh1595@gmail.com>
2024-03-06 13:16:00 +0530

    Configures local webpack dev setup and build.

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  target: "web",
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "main.js",
+  },
+  devServer: {
+    port: "8080",
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+};
