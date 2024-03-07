4e66dd6f772524aa4678882f1111fecd6d6e66bf
Sujesh95 <sujesh1595@gmail.com>
2024-03-07 10:15:29 +0530

    useState hook.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+
+const setInitialState = () => {
+  console.log("called multiple times");
+  return 4;
+};
 
 const App = () => {
-  return <h1>Is this working?</h1>;
+  const [count, setCount] = useState(setInitialState);
+
+  const incrementCounter = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrementCounter = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}>
+      <button
+        style={{
+          display: "inline-block",
+          width: "20px",
+          margin: "20px",
+          backgroundColor: "red",
+          border: "none",
+        }}
+        onClick={decrementCounter}>
+        -
+      </button>
+      <span>{count}</span>
+      <button
+        style={{
+          display: "inline-block",
+          width: "20px",
+          margin: "20px",
+          backgroundColor: "green",
+          border: "none",
+        }}
+        onClick={incrementCounter}>
+        +
+      </button>
+    </div>
+  );
 };
 
 export default App;
